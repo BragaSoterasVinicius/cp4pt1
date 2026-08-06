9a32140d88e4b086f2ae0e7afdaedcdedc4a56f0
BragaSoterasVinicius <viniciussoteras@gmail.com>
2026-08-06 20:10:18 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/_tdspo/CP4pt1/Cp4pt1Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap._tdspo.CP4pt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp4pt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp4pt1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/_tdspo/CP4pt1/Cp4pt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap._tdspo.CP4pt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp4pt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
