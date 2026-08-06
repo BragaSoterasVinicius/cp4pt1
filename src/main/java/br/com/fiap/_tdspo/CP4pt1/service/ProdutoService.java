e3797b45642e59184574fd971faabe7c3a7b00b0
BragaSoterasVinicius <viniciussoteras@gmail.com>
2026-08-06 20:24:54 -0300

    feat: layout do Controller e Service feitos

--- src/main/java/br/com/fiap/_tdspo/CP4pt1/service/ProdutoService.java
+++ src/main/java/br/com/fiap/_tdspo/CP4pt1/service/ProdutoService.java
@@ -1,4 +1,12 @@
 package br.com.fiap._tdspo.CP4pt1.service;
 
+import br.com.fiap._tdspo.CP4pt1.repository.ProdutoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
 }
